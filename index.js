9992bf5e932610f0693d5eec5fedb423973f9c00
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-09-19 06:33:38 +1000

    implemented basic timer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 const start = document.querySelector('.start');
-// const haveSomeHelp = setTimeout(1000, moveDiv);
+const timer = document.querySelector('.timer');
+let time = 0;
 
 
 function moveDiv (){
@@ -8,19 +9,23 @@ function moveDiv (){
     const popPosition2 = Math.random().toFixed(2)*500;
 container.style.marginLeft = `${popPosition + 1}px`;
 container.style.marginTop = `${popPosition2 +1}px`;
-
 };
 
 function startGame (){
     start.remove();
     setInterval(moveDiv, 800);
+    setInterval(timeElapsed, 1000);
 }
 
 function youWin (){
-    window.alert("YOU WIN!");
+    window.alert(`YOU WIN! It took you ${time} seconds.`);
     location.reload();
 }
 
+function timeElapsed (){
+    time++;
+    timer.innerText = time;
+}
 
 // container.addEventListener("mouseenter", moveDiv);
 container.addEventListener("click", youWin);
