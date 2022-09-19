0cabb41028293e017024abac17a256074888351e
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-09-20 06:45:46 +1000

    Commit 4 - working difficulty selector implemented

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const container = document.querySelector('.container');
-
 const timer = document.querySelector('.timer');
 const start = document.querySelectorAll('.difficultyButton');
 const difficultySelectDiv = document.querySelector('.difficulty')
@@ -7,6 +6,7 @@ const hideGame = document.querySelector('.hide-game')
 let time = 0;
 
 
+// MOVES TARGET
 function moveDiv (){
     const popPosition = Math.random().toFixed(2)*1000;
     const popPosition2 = Math.random().toFixed(2)*500;
@@ -15,36 +15,64 @@ container.style.marginTop = `${popPosition2 +1}px`;
 };
 
 
-function startGame (){
+const easy = document.querySelector('#easy');
+const normal = document.querySelector('#normal');
+const hard = document.querySelector('#hard');
+const impossible = document.querySelector('#impossible');
+easy.addEventListener('click', startGameEasy);
+normal.addEventListener ('click', startGameNormal);
+hard.addEventListener ('click', startGameHard);
+impossible.addEventListener('click', startGameImpossible);
+
+//STARTS GAME - obviously
+function startGameEasy (){
         setInterval(moveDiv, 800);
-    setInterval(timeElapsed, 1000);
+        setInterval(timeElapsed, 1000);
+        removeMenu();
 }
 
+function startGameNormal (){
+        setInterval(moveDiv, 600);
+        setInterval(timeElapsed, 1000);
+        removeMenu();
+}
 
+function startGameHard (){
+        setInterval(moveDiv, 500);
+        setInterval(timeElapsed, 1000);
+        removeMenu();
+}
+
+function startGameImpossible (){
+        container.addEventListener("mouseenter", moveDiv);
+        setInterval(timeElapsed, 1000);
+        removeMenu();
+    }
+
+
+
+//MESSAGE WHEN TARGET IS HIT
 function youWin (){
     window.alert(`YOU WIN! It took you ${time} seconds.`);
     location.reload();
 }
 
-
+//TIMER
 function timeElapsed (){
     time++;
     timer.innerText = time;
 }
 
-for (let i = 0; i < start.length; i++){
-start[i].addEventListener('click', selectDifficulty);
-}
 
-function selectDifficulty (){
+
+
+
+
+
+function removeMenu (){
     hideGame.style.visibility = 'visible';
-    startGame();
     difficultySelectDiv.remove();
 }
 
 
-
-
-// container.addEventListener("mouseenter", moveDiv);
-// start.addEventListener("click", startGame);
 container.addEventListener("click", youWin);
