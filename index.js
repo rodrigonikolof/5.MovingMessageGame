4f3ec97c99f541f92da555595de4eea109cce468
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-09-19 17:28:51 +1000

    Commit 3 - added difficulty selector

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('.container');
-const start = document.querySelector('.start');
+
 const timer = document.querySelector('.timer');
+const start = document.querySelectorAll('.difficultyButton');
+const difficultySelectDiv = document.querySelector('.difficulty')
+const hideGame = document.querySelector('.hide-game')
 let time = 0;
 
 
@@ -11,22 +14,37 @@ container.style.marginLeft = `${popPosition + 1}px`;
 container.style.marginTop = `${popPosition2 +1}px`;
 };
 
+
 function startGame (){
-    start.remove();
-    setInterval(moveDiv, 800);
+        setInterval(moveDiv, 800);
     setInterval(timeElapsed, 1000);
 }
+
 
 function youWin (){
     window.alert(`YOU WIN! It took you ${time} seconds.`);
     location.reload();
 }
 
+
 function timeElapsed (){
     time++;
     timer.innerText = time;
 }
 
+for (let i = 0; i < start.length; i++){
+start[i].addEventListener('click', selectDifficulty);
+}
+
+function selectDifficulty (){
+    hideGame.style.visibility = 'visible';
+    startGame();
+    difficultySelectDiv.remove();
+}
+
+
+
+
 // container.addEventListener("mouseenter", moveDiv);
+// start.addEventListener("click", startGame);
 container.addEventListener("click", youWin);
-start.addEventListener("click", startGame);
